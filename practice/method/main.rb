5b8f1941d795436e33a3e6455cc88b7d06e9ef85
並木　久稔 <namikihisatoshi@namikihisatoshinoMacBook-Pro.local>
2021-01-20 23:53:10 +0900

    旅行プランメソッド

---
+++ practice/method/main.rb
@@ -0,0 +1,17 @@
+require './method'
+
+plans =[
+  {name: "沖縄", price:10000},
+  {name: "北海道", price:20000},
+  {name: "九州", price:15000}
+]
+
+# 目次
+# プランを表示する
+disp_plan(plans)
+# プランを選択する
+chosen_plan = choose_plan(plans)
+# 人数を選択する
+chosen_num_of_people = choose_num_of_people(chosen_plan)
+# 合計金額を計算する
+calculate_total_price(chosen_plan, chosen_num_of_people)
